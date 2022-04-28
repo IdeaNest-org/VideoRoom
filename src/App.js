8879dad62d77c964a6d4b780c40c4c986509f879
pengzai <roc.liu@hotmail.cm>
2022-04-29 00:39:00 +0800

    同步代码

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import * as React from 'react';
 import StatusBar from './components/StatusBar.tsx';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
+            <div>
+                <video width="640" height="500" controls style={{
+                    backgroundColor: 'black'
+                }} src="https://www.youtube.com/watch?v"/>
+            </div>
             <StatusBar
                 style={{
                     position: 'fixed',
--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -13,11 +13,27 @@ function getUid() {
     }
     return userId;
 }
+function getRoomId() {
+    let roomId = localStorage.getItem('roomId');
+    if (!roomId) {
+        roomId = uuid.v4();
+        localStorage.setItem('userId', roomId);
+    }
+    return roomId;
+}
+function cleanRoomId() {
+    localStorage.setItem('roomId', '');
+}
 export default function StatusBar({ ...props }) {
     const [isOpen, setIsOpen] = useState(false);
     const [status, setStatus] = useState('offline');
+    const [roomId, setRoomId] = useState('');
 
     useEffect(() => {
+        connect();
+    }, []);
+    // 链接
+    const connect = () => {
         goEasy.connect({
             id: getUid(), //pubsub选填，im必填，最大长度60字符
             data: { id: getUid() }, //必须是一个对象，pubsub选填，im必填，最大长度300字符，用于上下线提醒和查询在线用户列表时，扩展更多的属性
@@ -35,7 +51,10 @@ export default function StatusBar({ ...props }) {
         return () => {
             goEasy.disconnect({});
         };
-    }, []);
+    };
+    // 当前房间的状态
+    // 是否房主
+    // 当前视频，url ，状态，时间
     const toggle = () => {
         setIsOpen(!isOpen);
     };
@@ -49,14 +68,17 @@ export default function StatusBar({ ...props }) {
                     variant="contained"
                     aria-label="outlined primary button group"
                 >
-                    <Button>分享</Button>
+                    <Button onClick={() => {}}>分享</Button>
                     <Button>状态</Button>
-                    {/* <Button>聊天</Button> */}
                 </ButtonGroup>
             )}
-            <Fab onClick={toggle} aria-label="like" color="primary">
+            <Fab
+                disabled={status !== 'online'}
+                onClick={toggle}
+                aria-label="like"
+                color="primary"
+            >
                 <FavoriteIcon />
-                {status}
             </Fab>
         </Box>
     );
